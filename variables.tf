7f6980d84a19d0a81b45cdc817b88a2a71589580
Dennis Mastin <dennis.mastin@NG-DENNIS-M.local>
2022-10-11 13:19:39 -0600

    updated variables.tf and main.tf for conjur provider

--- variables.tf
+++ variables.tf
@@ -1,3 +1,23 @@
+provider "conjur" {}
+
+data "conjur_secret" "aws_access_key_id" {
+  name = "aws-access-key"
+}
+
+data "conjur_secret" "aws_secret_key" {
+  name = "aws-secret-key"
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  default = "${data.conjur_secret.aws_access_key_id.value}"
+}
+
+variable "aws_secret_key" {
+  description = "AWS Access Key ID"
+  default = "${data.conjur_secret.aws_secret_key.value}"
+}
+
 variable "region" {
   description = "AWS region"
   default     = "us-west-1"
