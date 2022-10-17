7d616a85e5dad590186dd19c63e36ee648c51478
Dennis Mastin <dennis.mastin@NG-DENNIS-M.local>
2022-10-17 09:51:06 -0600

    working example deployed; CONJUR_ACCOUNT=conjur *always* for c-cloud

--- get-x509.sh
+++ get-x509.sh
@@ -4,6 +4,7 @@
 
 FQDN=sempra-p.secretsmgr.cyberark.cloud
 
-openssl s_client -servername $FQDN -connect $FQDN:443 2>/dev/null | \
-    openssl x509 -text
+#openssl s_client -servername $FQDN -connect $FQDN:443 2>/dev/null | \
+#    openssl x509 -text
 
+    openssl s_client -showcerts -connect $FQDN:443  < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p'
--- tf-env-vars.sh
+++ tf-env-vars.sh
@@ -2,8 +2,8 @@
 
 # These should be set as env vars in both standalone and tf-cloud - category: env
 # The certfile needs to be on disk - for tf cloud, ?checked into git?
-export CONJUR_CERT_FILE=./cert-file.pem
+export CONJUR_CERT_FILE="./conjur-cloud-sempra.pem"
 export CONJUR_AUTHN_API_KEY="<<<<api-key-for-conjur-host-below>>>"
 export CONJUR_AUTHN_LOGIN="host/tf-host-aws"
-export CONJUR_ACCOUNT="conjur-account-name"
+export CONJUR_ACCOUNT="conujur"  # Always 'conjur' for conjur-cloud
 export CONJUR_APPLIANCE_URL="https://ec2-xx-xxx-xx-xxx.us-west-1.compute.amazonaws.com"
