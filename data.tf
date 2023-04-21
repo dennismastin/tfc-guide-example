14f0a2782a6a14579d14acfd513420c768213fd6
Dennis Mastin <dennis.mastin@NG-DENNIS-M.local>
2023-04-21 13:04:19 -0600

    spaces, baby

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
 data "template_file" "user_data" {
   template = file("${path.module}/user_data.tftpl")
   vars = {
-    TEMPLATE_FLOWS_ADMIN_PWD = var.FLOWS_ADMIN_PWD
-    SSH_PKEY = module.key_pair.private_key_pem
-    SSH_ADDRESS = aws_instance.ubuntu.public_ip
+    TEMPLATE_FLOWS_ADMIN_PWD = "var.FLOWS_ADMIN_PWD"
+    SSH_PKEY = "module.key_pair.private_key_pem"
+    SSH_ADDRESS = "aws_instance.ubuntu.public_ip"
     
   }
 }
