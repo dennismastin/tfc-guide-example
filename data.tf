7ba2f8062b6e20350357e3c96bbbf0706a74a60d
Dennis Mastin <dennis.mastin@NG-DENNIS-M.local>
2023-04-21 12:56:18 -0600

    first run at user_data with vars

--- data.tf
+++ data.tf
@@ -1,7 +1,9 @@
 data "template_file" "user_data" {
   template = file("${path.module}/user_data.tftpl")
   vars = {
-    var1  = "var1"
+    TEMPLATE_FLOWS_ADMIN_PWD=var.FLOWS_ADMIN_PWD
+    SSH_PKEY=module.key_pair.private_key_pem
+    SSH_ADDRESS=aws_instance.ubuntu.public_ip
     
   }
 }
