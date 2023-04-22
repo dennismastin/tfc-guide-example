d3d70837c676617f3676e63ffda7161292c30023
Dennis Mastin <dennis.mastin@NG-DENNIS-M.local>
2023-04-22 14:37:54 -0600

    revert out of pkcs

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "instance_ipaddr" {
 }
 
 output "instance_ssh_private" {
-  value = module.key_pair.private_key_pem_pkcs8
+  value = module.key_pair.private_key_pem
   sensitive = true
 }
 
